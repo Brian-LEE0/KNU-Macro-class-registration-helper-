717eec9deab849216c7763003d0dfe5e851c3265
NNAisle <liy908@naver.com>
2022-08-10 03:20:07 +0900

    verfinal

--- source20220808/knu.py
+++ source20220808/knu.py
@@ -1,23 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 import sys, os
 import datetime
 from time import sleep
 import requests
+from requests.structures import CaseInsensitiveDict
+import json
 
 TOKEN = ''
 temp = ''
-driver = ''
-
-def init() :
-	global driver
-	chrome_options = webdriver.ChromeOptions()
-	chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-	chrome_options.add_argument("headless")
-	driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=chrome_options)
-	
+index = 0
 
 def countdown(t):
 	try :
@@ -33,39 +23,84 @@ def countdown(t):
 	except Exception as ex:
 		print(f'ERROR : {ex}')
 
-def crawling(subj) :
-	global driver
+
+def req_info_subjcode(year,semester_cd,subj_cd) :
+	url = "https://knuin.knu.ac.kr/public/web/stddm/lsspr/syllabus/lectPlnInqr/selectListLectPlnInqr"
+
+	headers = CaseInsensitiveDict()
+	headers["Content-Type"] = "application/json"
+	
+	data = {
+		"search":
+		{"estblYear":"",
+		"estblSmstrSctcd":"",
+		"sbjetCd":"",
+		"sbjetNm":"",
+		"crgePrfssNm":"",
+		"sbjetRelmCd":"",
+		"sbjetSctcd":"",
+		"estblDprtnCd":"",
+		"rmtCrseYn":"",
+		"rprsnLctreLnggeSctcd":"",
+		"flplnCrseYn":"",
+		"pstinNtnnvRmtCrseYn":"",
+		"dgGbDstrcRmtCrseYn":"",
+		"sugrdEvltnYn":"",
+		"gubun":"01",
+		"isApi":"Y",
+		"bldngSn":"",
+		"bldngCd":"",
+		"bldngNm":"",
+		"lssnsLcttmUntcd":"",
+		"sbjetSctcd2":"",
+		"contents":"",
+		"lctreLnggeSctcd":"ko"}
+	}
+	data["search"]["estblYear"] = year
+	data["search"]["estblSmstrSctcd"] = semester_cd
+	data["search"]["sbjetCd"] = subj_cd[0:8]
+	data["search"]["contents"] = subj_cd[0:8]
+	resp = requests.post(url, headers=headers, data=json.dumps(data))
+	return json.loads(resp.text)
+
+def init_crawling(year,semester_cd,subj_cd):
 	try :
-		driver.set_window_size(3000, 5000)
-		driver.get('https://knuin.knu.ac.kr/public/stddm/lectPlnInqr.knu')
-		driver.implicitly_wait(0.1)
-		driver.find_element(By.XPATH,'//*[@id="schCode"]/option[2]').click()
-		driver.implicitly_wait(0.1)
-		driver.find_element(By.XPATH,'//*[@id="schCodeContents"]').send_keys(subj[0:8])
-		driver.implicitly_wait(0.1)
-		driver.find_element(By.XPATH,'//*[@id="btnSearch"]').click()
-		driver.implicitly_wait(3)
-		subj_cd = driver.find_element(By.XPATH,'//*[@id="grid01_cell_'+ str(int(subj[9:])-1)+'_6"]/nobr').text
-		subj_name = driver.find_element(By.XPATH,'//*[@id="grid01_cell_'+ str(int(subj[9:])-1)+'_7"]/nobr').text
-		subj_limit = driver.find_element(By.XPATH,'//*[@id="grid01_cell_'+ str(int(subj[9:])-1)+'_16"]/nobr').text
-		subj_current = driver.find_element(By.XPATH,'//*[@id="grid01_cell_'+ str(int(subj[9:])-1)+'_17"]/nobr').text
+		global index
+		subj_info = (req_info_subjcode(year,semester_cd,subj_cd))["data"]
+
+		for data in subj_info :
+			if(data["crseNo"] == subj_cd) :
+				return data[index]
+			else :
+				index+=1
+		return -1
+	except Exception as ex :
+		return -1
+
+
+
+def crawling(year,semester_cd,subj_cd) :
+	try :
+		subj_info = (req_info_subjcode(year,semester_cd,subj_cd))["data"][index]
 
 		return {
-			'subj_name' : subj_name,
-			'subj_cd' : subj_cd,
-			'subj_limit' : int(subj_limit),
-			'subj_current' : int(subj_current)
+			'subj_name' : subj_info["sbjetNm"],
+			'subj_cd' : subj_info["crseNo"],
+			'subj_limit' : int(subj_info["attlcPrscpCnt"]),
+			'subj_current' : int(subj_info["appcrCnt"])
 		}
 		
 	except Exception as ex :
-		print(f'ERROR : 올바른 과목코드가 아닙니다 다시 확인해 주세요!')
+		return -1
+
+		
 
 
-def init_req(**sub):
+def init_req(sub):
 	try :
 	    TARGET_URL = 'https://notify-api.line.me/api/notify'
 	    # 요청합니다.
-	    mes={'message': sub['subj_name'] +'('+ sub['subj_cd'] +')'+'과목의 정원변경 추적을 시작합니다.'}
+	    mes={'message': sub['sbjetNm'] +'('+ sub['crseNo'] +')'+'과목의 정원변경 추적을 시작합니다.'}
 	    response = requests.post(
 	    TARGET_URL,
 	    headers={
@@ -77,7 +112,7 @@ def init_req(**sub):
 	except :
 		print(f'ERROR : 올바른 토큰이 아닙니다!')
 
-def req(**sub):
+def req(sub):
 	try :
 	    TARGET_URL = 'https://notify-api.line.me/api/notify'
 	    # 요청합니다.
@@ -94,10 +129,13 @@ def req(**sub):
 		print(f'ERROR : 올바른 토큰이 아닙니다!')
 
 if __name__ == "__main__":
-	std = 0
-	init()
+	std = -1
+	print("현재 사용하시는 프로그램은 최종 판으로, 학교 사이트가 다시 개편되기 전에는 계속 사용하실 수 있습니다")
+	print("이 프로그램은 전자공학부 2016학번 이 아무개가 만든 프로그램입니다. 자유롭게 사용하셔도 됩니다^^")
 	try :
 		print("#######################################")
+		year = input("년도를 입력해주세요 (ex : 2022) : ")
+		semester = int(input("학기를 입력해 주세요 (1학기는 1, 2학기는 2, 여름계절은 3, 겨울계절은 4) : "))
 		subj_cd = input("과목코드를 입력해 주세요 (ex : CLTR0023-001) : ").upper()
 		while 1 : 
 			if len(subj_cd) != 12 :
@@ -110,12 +148,26 @@ if __name__ == "__main__":
 	except Exception as ex :
 		print(f'ERROR : {ex}')
 
-	subj = crawling(subj_cd)
-	init_req(**subj)
+	if(semester == 1):
+		semester_cd = "CMBS001400001"
+	elif(semester == 2):
+		semester_cd = "CMBS001400002"
+	elif(semester == 3):
+		semester_cd = "CMBS001400004"
+	elif(semester == 4):
+		semester_cd = "CMBS001400003"
+
+	subj = crawling(year,semester_cd,subj_cd)
+	if(subj == -1) :
+		print(f'ERROR : 올바른 과목코드가 아닙니다 다시 확인해 주세요!')
+		countdown(5)
+		exit(0)
+
+	init_req(subj)
 
 	while 1 :
 		try : 
-			subj = crawling(subj_cd)
+			subj = crawling(year,semester_cd,subj_cd)
 			print(subj)
 			if std != subj['subj_current'] :
 				std = subj['subj_current']
@@ -125,9 +177,6 @@ if __name__ == "__main__":
 						countdown(2)
 			countdown(5)
 		except Exception as ex :
-			driver.close()
-			print('드라이버 재실행 중')
-			init()
 			countdown(5)
 
 	
